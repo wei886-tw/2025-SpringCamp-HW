d029af271028bcbc6ebe8d846a5d269adcdfa32f
Wei <lunhowl@gmail.com>
2025-06-06 16:59:47 +0800

    [complete] 首頁

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import './assets/all.scss'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+
 
 
 createApp(App).use(router).mount('#app')
